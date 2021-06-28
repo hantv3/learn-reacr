3ac3909aa556471637d6f63e13929f448dacab45
hantv15 <57491771+hantv15@users.noreply.github.com>
2021-06-28 15:50:07 +0700

    reacr  được push vào ngày 28/tháng 6/ 2021

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Truong Van Han
         </p>
         <a
           className="App-link"
